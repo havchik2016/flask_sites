983c796c8ded87fef157dc38720dea6bd96e72ed
havchik2016 <37791548+havchik2016@users.noreply.github.com>
2020-03-01 18:21:20 +1000

    task 5

--- site5.py
+++ site5.py
@@ -94,14 +94,81 @@ def selection_form():
                             <h1>Анкета претендента на участие в миссии</h1>
                             <div>
                                 <form class="login_form" method="post">
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите фамилию" name="email">
-                                    <input type="password" class="form-control" id="password" placeholder="Введите имя" name="password">
+                                    <textarea class="form-control" id="about" rows="1" name="surname" placeholder="Введите фамилию"></textarea>
+                                    <textarea class="form-control" id="about" rows="1" name="name" placeholder="Введите имя"></textarea>
+                                    <br>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у Вас образование?</label>
+                                        <select class="form-control" id="classSelect" name="education">
+                                          <option>Начальное</option>
+                                          <option>Основное среднее</option>
+                                          <option>Среднее профессиональное</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                     </div> 
+                                     Какие у Вас есть профессии?
+                                     <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="prof1" name="accept1">
+                                        <label class="form-check-label" for="prof1">Инженер-исследователь</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof2" name="accept2">
+                                        <label class="form-check-label" for="prof2">Инженер-строитель</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof3" name="accept3">
+                                        <label class="form-check-label" for="prof3">Пилот</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof4" name="accept4">
+                                        <label class="form-check-label" for="prof4">Метеоролог</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof5" name="accept5">
+                                        <label class="form-check-label" for="prof5">Инженер по жизнеобеспечению</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof6" name="accept6">
+                                        <label class="form-check-label" for="prof6">Инженер по радиационной защите</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof7" name="accept7">
+                                        <label class="form-check-label" for="prof7">Врач</label>
+                                        <br>
+                                        <input type="checkbox" class="form-check-input" id="prof8" name="accept8">
+                                        <label class="form-check-label" for="prof8">Экзобиолог</label>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему Вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                    <label for="photo">Приложите фотографию</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
                                 </form>
                             </div>
                         </body>
                     </html>'''
     elif request.method == "POST":
-        pass
+        return "Форма отправлена успешно!"
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
